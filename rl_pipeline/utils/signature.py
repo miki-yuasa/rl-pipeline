6aa8ee43dae6e57680bdcce94487acab4b384e90
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-08-19 18:15:26 -0500

    feat: add signature moduel

---
+++ rl_pipeline/utils/signature.py
@@ -0,0 +1,12 @@
+import uuid
+from datetime import datetime
+
+
+def unique_id() -> str:
+    """Generate a unique identifier for the training run."""
+    return str(uuid.uuid4())[:4]
+
+
+def exp_time(self) -> str:
+    """Get the current time formatted for the experiment."""
+    return datetime.now().strftime("%m-%d_%H-%M-%S.%f")
