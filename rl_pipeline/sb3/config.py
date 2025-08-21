97d25c27a2510ba354450c65dd71a07301295219
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-08-21 17:11:05 -0500

    feat: add config reader

--- rl_pipeline/sb3/config.py
+++ rl_pipeline/sb3/config.py
@@ -1,22 +1,24 @@
-import os
 from typing import Any
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.type_aliases import MaybeCallback
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
-from rl_pipeline.core.config import ConfigReader, SaveConfig, SaveConfigReader
-from rl_pipeline.core.utils.io import format_large_number, read_config_dict_from_yaml
+from rl_pipeline.core.config import SaveConfig
 from rl_pipeline.gymnasium.config import MakeEnvConfig, WrapperConfig
 
-from .callback import CheckpointCallbackConfig, EvalCallbackConfig
+from .callback import (
+    CheckpointCallbackConfig,
+    EvalCallbackConfig,
+    VideoRecorderCallbackConfig,
+)
 
 
 class MakeVecEnvConfig(BaseModel):
-    n_envs: int = 1
+    n_envs: int = Field(ge=1, default=1)
     seed: int | None = None
-    start_index: int = 0
+    start_index: int = Field(ge=0, default=0)
     monitor_dir: str | None = None
     env_kwargs: dict[str, Any] | None = None
     vec_env_cls: type[SubprocVecEnv] | type[DummyVecEnv] | None = SubprocVecEnv
@@ -33,9 +35,9 @@ class SB3AlgorithmConfig(BaseModel):
 
 
 class SB3LearnConfig(BaseModel):
-    total_timesteps: int
-    callback: MaybeCallback = None
-    log_interval: int = 100
+    total_timesteps: int = Field(ge=1, default=1_000_000)
+    # callback: MaybeCallback = None
+    log_interval: int = Field(ge=0, default=100)
     tb_log_name: str = "run"
     reset_num_timesteps: bool = True
     progress_bar: bool = False
@@ -44,9 +46,10 @@ class SB3LearnConfig(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
-class SB3CallbackConfigs(BaseModel):
+class SB3CallbackConfig(BaseModel):
     eval_callback_config: EvalCallbackConfig
     ckpt_callback_config: CheckpointCallbackConfig
+    video_recorder_callback_config: VideoRecorderCallbackConfig | None = None
 
 
 class SB3PipelineConfig(BaseModel):
@@ -59,72 +62,11 @@ class SB3PipelineConfig(BaseModel):
     vec_config: MakeVecEnvConfig | None = None
     algo_config: SB3AlgorithmConfig
     learn_config: SB3LearnConfig
-    callback_configs: SB3CallbackConfigs
+    callback_config: SB3CallbackConfig
 
 
-class SB3ConfigReader(BaseModel, ConfigReader[SB3PipelineConfig]):
-    """Configuration reader for SB3 pipeline."""
-
-    device: str | int = "cuda:0"
-    experiment_id: str = ""
-    retrain_model: bool = True
-    save_config: SaveConfigReader
-    config_dir: str = "configs"
-    env_config_file: str = "env_config.yaml"
-    wrapper_config_file: str | None = None
-    vec_config_file: str | None = None
-    algo_config_file: str = "algo_config.yaml"
-    learn_config_file: str = "learn_config.yaml"
-    callback_configs_file: str = "callback_configs.yaml"
-
-    def to_config(self) -> SB3PipelineConfig:
-        device: str = (
-            self.device if isinstance(self.device, str) else f"cuda:{self.device}"
-        )
-
-        env_config: MakeEnvConfig = read_config_dict_from_yaml(
-            self.config_dir, self.env_config_file, MakeEnvConfig
-        )
-        wrapper_config: WrapperConfig | None = (
-            read_config_dict_from_yaml(
-                self.config_dir, self.wrapper_config_file, WrapperConfig
-            )
-            if self.wrapper_config_file
-            else None
-        )
-        vec_config: MakeVecEnvConfig | None = (
-            read_config_dict_from_yaml(
-                self.config_dir, self.vec_config_file, MakeVecEnvConfig
-            )
-            if self.vec_config_file
-            else None
-        )
-        algo_config: SB3AlgorithmConfig = read_config_dict_from_yaml(
-            self.config_dir, self.algo_config_file, SB3AlgorithmConfig
-        )
-        learn_config: SB3LearnConfig = read_config_dict_from_yaml(
-            self.config_dir, self.learn_config_file, SB3LearnConfig
-        )
-        callback_configs: SB3CallbackConfigs = read_config_dict_from_yaml(
-            self.config_dir, self.callback_configs_file, SB3CallbackConfigs
-        )
-
-        save_config: SaveConfig = self.save_config.to_config(
-            experiment_id=self.experiment_id,
-            model_name_suffix="_" + format_large_number(learn_config.total_timesteps),
-        )
-
-        pipeline_config = SB3PipelineConfig(
-            device=device,
-            experiment_id=self.experiment_id,
-            retrain_model=self.retrain_model,
-            save_config=save_config,
-            env_config=env_config,
-            wrapper_config=wrapper_config,
-            vec_config=vec_config,
-            algo_config=algo_config,
-            learn_config=learn_config,
-            callback_configs=callback_configs,
-        )
-
-        return pipeline_config
+class SB3ModelConfig(BaseModel):
+    algo_config: SB3AlgorithmConfig
+    learn_config: SB3LearnConfig
+    vec_config: MakeVecEnvConfig | None = None
+    callback_config: SB3CallbackConfig
---
+++ rl_pipeline/sb3/config_reader.py
@@ -0,0 +1,263 @@
+import os
+from typing import Any
+
+from pydantic import BaseModel, Field
+from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+
+from rl_pipeline.core.config import ConfigReader, SaveConfig, SaveConfigReader
+from rl_pipeline.core.utils.io import (
+    format_large_number,
+    get_class,
+    read_config_dict_from_yaml,
+)
+from rl_pipeline.gymnasium.config import MakeEnvConfig, WrapperConfig
+
+from .callback import (
+    CheckpointCallbackConfig,
+    EvalCallbackConfig,
+    VideoRecorderCallbackConfig,
+)
+from .config import (
+    MakeVecEnvConfig,
+    SB3AlgorithmConfig,
+    SB3CallbackConfig,
+    SB3LearnConfig,
+    SB3ModelConfig,
+    SB3PipelineConfig,
+)
+
+
+class SB3AlgorithmConfigReader(BaseModel, ConfigReader[SB3AlgorithmConfig]):
+    algorithm: str = "PPO"
+    algo_kwargs: dict[str, Any] = {}
+
+    def to_config(self) -> SB3AlgorithmConfig:
+        algo_class: type[BaseAlgorithm] | None = get_class(
+            "stable_baselines3." + self.algorithm
+        )
+        assert algo_class is not None, (
+            f"Could not find algorithm class for {self.algorithm}"
+        )
+        return SB3AlgorithmConfig(
+            algorithm=algo_class,
+            algo_kwargs=self.algo_kwargs,
+        )
+
+
+class SB3LearnConfigReader(BaseModel, ConfigReader[SB3LearnConfig]):
+    total_timesteps: int = Field(ge=1, default=1_000_000)
+    log_interval: int = Field(ge=0, default=100)
+    tb_log_name: str = "run"
+    reset_num_timesteps: bool = True
+    progress_bar: bool = False
+
+    def to_config(self) -> SB3LearnConfig:
+        return SB3LearnConfig(
+            total_timesteps=self.total_timesteps,
+            log_interval=self.log_interval,
+            tb_log_name=self.tb_log_name,
+            reset_num_timesteps=self.reset_num_timesteps,
+            progress_bar=self.progress_bar,
+        )
+
+
+class MakeVecEnvConfigReader(BaseModel, ConfigReader[MakeVecEnvConfig]):
+    n_envs: int = Field(ge=1, default=1)
+    seed: int | None = None
+    start_index: int = Field(ge=0, default=0)
+    monitor_dir: str | None = None
+    env_kwargs: dict[str, Any] | None = None
+    vec_env_cls: str | None = "SubprocVecEnv"
+    vec_env_kwargs: dict[str, Any] | None = None
+    monitor_kwargs: dict[str, Any] | None = None
+
+    def to_config(self) -> MakeVecEnvConfig:
+        vec_env_cls: type[SubprocVecEnv] | type[DummyVecEnv] | None = (
+            get_class("stable_baselines3.common.vec_env." + self.vec_env_cls)
+            if self.vec_env_cls
+            else None
+        )
+        return MakeVecEnvConfig(
+            n_envs=self.n_envs,
+            seed=self.seed,
+            start_index=self.start_index,
+            monitor_dir=self.monitor_dir,
+            env_kwargs=self.env_kwargs,
+            vec_env_cls=vec_env_cls,
+            vec_env_kwargs=self.vec_env_kwargs,
+            monitor_kwargs=self.monitor_kwargs,
+        )
+
+
+class EvalCallbackConfigReader(BaseModel):
+    eval_freq: int = Field(ge=0)
+    n_eval_episodes: int = Field(ge=1)
+    log_path: str = "eval"
+    deterministic: bool = False
+    render: bool = False
+
+    def to_config(self, save_config: SaveConfig) -> EvalCallbackConfig:
+        return EvalCallbackConfig(
+            eval_freq=self.eval_freq,
+            n_eval_episodes=self.n_eval_episodes,
+            best_model_save_path=save_config.model_save_dir,
+            log_path=os.path.join(save_config.model_save_dir, self.log_path),
+            deterministic=self.deterministic,
+            render=self.render,
+        )
+
+
+class CheckpointCallbackConfigReader(BaseModel):
+    save_freq: int = Field(ge=1, default=100)
+    save_path: str = "ckpts"
+    name_prefix: str = "ckpt"
+    save_replay_buffer: bool = False
+    verbose: int = 0
+
+    def to_config(self, save_config: SaveConfig) -> CheckpointCallbackConfig:
+        return CheckpointCallbackConfig(
+            save_freq=self.save_freq,
+            save_path=os.path.join(save_config.model_save_dir, self.save_path),
+            name_prefix=self.name_prefix,
+            save_replay_buffer=self.save_replay_buffer,
+            verbose=self.verbose,
+        )
+
+
+class VideoRecorderCallbackConfigReader(BaseModel):
+    render_freq: int = Field(ge=1, default=100)
+    save_dir: str = "ckpts"
+    name_prefix: str = "rl_model"
+    file_ext: str = "gif"
+    deterministic: bool = False
+
+    def to_config(self, save_config: SaveConfig) -> VideoRecorderCallbackConfig:
+        return VideoRecorderCallbackConfig(
+            render_freq=self.render_freq,
+            save_dir=os.path.join(save_config.model_save_dir, self.save_dir),
+            name_prefix=self.name_prefix,
+            file_ext=self.file_ext,
+            deterministic=self.deterministic,
+        )
+
+
+class SB3CallbackConfigReader(BaseModel):
+    eval_callback_config: EvalCallbackConfigReader
+    ckpt_callback_config: CheckpointCallbackConfigReader = (
+        CheckpointCallbackConfigReader()
+    )
+    video_recorder_callback_config: VideoRecorderCallbackConfigReader | None = None
+
+    def to_config(self, save_config: SaveConfig) -> SB3CallbackConfig:
+        eval_callback_config = self.eval_callback_config.to_config(
+            save_config=save_config
+        )
+        ckpt_callback_config = self.ckpt_callback_config.to_config(
+            save_config=save_config
+        )
+        video_recorder_callback_config = (
+            self.video_recorder_callback_config.to_config(save_config=save_config)
+            if self.video_recorder_callback_config
+            else None
+        )
+
+        return SB3CallbackConfig(
+            eval_callback_config=eval_callback_config,
+            ckpt_callback_config=ckpt_callback_config,
+            video_recorder_callback_config=video_recorder_callback_config,
+        )
+
+
+class SB3ModelConfigReader(BaseModel):
+    """Configuration reader for SB3 model."""
+
+    algo_config: SB3AlgorithmConfigReader = SB3AlgorithmConfigReader()
+    learn_config: SB3LearnConfig = SB3LearnConfig()
+    vec_config: MakeVecEnvConfigReader | None = None
+    callback_config: SB3CallbackConfigReader
+
+    def to_config(self, save_config: SaveConfig) -> SB3ModelConfig:
+        return SB3ModelConfig(
+            algo_config=self.algo_config.to_config(),
+            learn_config=self.learn_config,
+            vec_config=self.vec_config.to_config() if self.vec_config else None,
+            callback_config=self.callback_config.to_config(save_config=save_config),
+        )
+
+
+class SB3ConfigReader(BaseModel, ConfigReader[SB3PipelineConfig]):
+    """Configuration reader for SB3 pipeline."""
+
+    device: str | int = "cuda:0"
+    experiment_id: str = ""
+    retrain_model: bool = True
+    save_config: SaveConfigReader
+    config_dir: str = "configs"
+    env_config_file: str = "env_config.yaml"
+    wrapper_config_file: str | None = None
+    model_config_file: str = "model_config.yaml"
+
+    def to_config(self) -> SB3PipelineConfig:
+        device: str = (
+            self.device if isinstance(self.device, str) else f"cuda:{self.device}"
+        )
+
+        env_config: MakeEnvConfig = self._to_env_config()
+        wrapper_config: WrapperConfig | None = self._to_wrapper_config()
+
+        model_config_reader: SB3ModelConfigReader = self._to_model_config_reader()
+
+        save_config: SaveConfig = self._to_save_config()
+
+        model_config: SB3ModelConfig = model_config_reader.to_config(
+            save_config=save_config
+        )
+
+        vec_config = model_config.vec_config
+        algo_config = model_config.algo_config
+        learn_config = model_config.learn_config
+        callback_config = model_config.callback_config
+
+        pipeline_config = SB3PipelineConfig(
+            device=device,
+            experiment_id=self.experiment_id,
+            retrain_model=self.retrain_model,
+            save_config=save_config,
+            env_config=env_config,
+            wrapper_config=wrapper_config,
+            vec_config=vec_config,
+            algo_config=algo_config,
+            learn_config=learn_config,
+            callback_config=callback_config,
+        )
+
+        return pipeline_config
+
+    def _to_env_config(self) -> MakeEnvConfig:
+        env_config: MakeEnvConfig = read_config_dict_from_yaml(
+            self.config_dir, self.env_config_file, MakeEnvConfig
+        )
+        return env_config
+
+    def _to_wrapper_config(self) -> WrapperConfig | None:
+        if self.wrapper_config_file:
+            return read_config_dict_from_yaml(
+                self.config_dir, self.wrapper_config_file, WrapperConfig
+            )
+        return None
+
+    def _to_model_config_reader(self) -> SB3ModelConfigReader:
+        model_config_reader: SB3ModelConfigReader = read_config_dict_from_yaml(
+            self.config_dir, self.model_config_file, SB3ModelConfigReader
+        )
+
+        return model_config_reader
+
+    def _to_save_config(self) -> SaveConfig:
+        model_config_reader = self._to_model_config_reader()
+        return self.save_config.to_config(
+            experiment_id=self.experiment_id,
+            model_name_suffix="_"
+            + format_large_number(model_config_reader.learn_config.total_timesteps),
+        )
